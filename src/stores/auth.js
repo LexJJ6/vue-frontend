65f01e57f605cee35063b4eea585603f7815f5d5
João Pimenta <joaoalexcp1717@gmail.com>
2026-01-31 17:19:02 +0000

    fix: Logout com autenticação stateful

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -2,8 +2,6 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 import { api } from '@/axios';
 
-// a utilizar localstorage de momento
-
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
@@ -18,31 +16,13 @@ export const useAuthStore = defineStore('auth', {
       // const res = await api.post('/login', { email, password });
       const res = await axios.post('http://localhost:8000/login', { email, password });
       console.log("dados do login", res.data);
-      // this.token = res.data.token;
-      // this.user = res.data.user;
-
-      // localStorage.setItem('token', this.token);
-
-      // configura header para futuras requests
-      // axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
     },
     async logout()
     {
-      // await axios.post('http://localhost:8000/api/logout', null, {
-      /*
-      await api.post('/logout', null, {
-        headers: { Authorization: `Bearer ${this.token}` }
-      });
-      */
-      await api.post('/logout');
-      // this.token = null;
-      // this.user = null;
-
-      // localStorage.removeItem('token');
+      await axios.post('http://localhost:8000/logout');
     },
     getAuthToken()
     {
-      // return this.token;
       const token = localStorage.getItem('token');
       return token;
     }
